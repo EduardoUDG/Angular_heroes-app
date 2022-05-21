3696a22b4abf5d3611d8160a5a4d7a45c87ca2a9
eduardo_chavez <eduardo.dorado@sanmina.com>
2022-05-20 22:59:59 -0500

    create - heroes-routing module

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,31 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HeroComponent } from './pages/hero/hero.component';
+import { SearchComponent } from './pages/search/search.component';
+import { AddComponent } from './pages/add/add.component';
+import { ListComponent } from './pages/list/list.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    children: [
+      { path: 'list',     component: ListComponent },
+      { path: 'add',      component: AddComponent },
+      { path: 'edit/:id', component: AddComponent },
+      { path: 'search',   component: SearchComponent },
+      { path: ':id',      component: HeroComponent },
+      { path: '**',       redirectTo: 'list' }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class HeroesRoutingModule { }
