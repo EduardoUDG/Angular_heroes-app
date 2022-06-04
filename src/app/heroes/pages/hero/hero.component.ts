03b0b301ec60b9cbdfabc7f7a3cbe0489874c1e7
eduardo_chavez <eduardo.dorado@sanmina.com>
2022-06-04 17:34:05 -0500

    update - hero design and router to back heroes list

--- src/app/heroes/pages/hero/hero.component.ts
+++ src/app/heroes/pages/hero/hero.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
 import { Hero } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
@@ -7,15 +7,20 @@ import { HeroesService } from '../../services/heroes.service';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styles: [
-  ]
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 10px;
+    }
+  `]
 })
 export class HeroComponent implements OnInit {
 
   hero!: Hero;
   constructor(
     private _activatedRoute: ActivatedRoute,
-    private _heroService: HeroesService
+    private _heroService: HeroesService,
+    private _router: Router
   ) { }
 
   ngOnInit(): void {
@@ -28,8 +33,10 @@ export class HeroComponent implements OnInit {
         this.hero = hero;
       }
     })
-
-
-
   }
+
+  toBack():void {
+    this._router.navigate(['/heroes/list']);
+  }
+
 }
