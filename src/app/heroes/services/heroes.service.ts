aa5c95b150cc7243c2f153b9c45d8b3ca180ed09
eduardo_chavez <eduardo.dorado@sanmina.com>
2022-06-04 17:50:24 -0500

    update - enviroment to dev and their servies ( hero )

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,20 +3,22 @@ import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 import { Hero } from './../interfaces/hero.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private _BASE_URL = environment.BASE_URL;
   constructor( private http: HttpClient ) { }
 
   getHeroes():Observable<Hero[]> {
-    return this.http.get<Hero[]>('http://localhost:3000/heroes');
+    return this.http.get<Hero[]>(`${this._BASE_URL}/heroes`);
   }
 
   getHeroById( id:string ):Observable<Hero> {
-    return this.http.get<Hero>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Hero>(`${this._BASE_URL}/heroes/${id}`);
   }
 
 }
